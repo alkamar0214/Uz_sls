4a6cb5ed10fab48ec414c2465a8629b191daaecd
K <40085614+kusman28@users.noreply.github.com>
2020-01-20 13:23:28 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   bool _rememberMe = false;
-  TextEditingController controllerEmail = new TextEditingController();
+  TextEditingController controllerUser = new TextEditingController();
   TextEditingController controllerPass = new TextEditingController();
 
   String msg = '';
